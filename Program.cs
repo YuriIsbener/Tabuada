d602a1a0f7f9132db29495262ae59c2997b3992c
Yuri Isbener <yurisuhett@gmail.com>
2020-11-17 17:48:45 -0300

    Atividade feita

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número?");
+            int numero = int.Parse(Console.ReadLine());
+            int resultado;
+
+            for(var contador = 0; contador <= 10; contador++)
+            {
+             resultado = numero*contador;
+             Console.WriteLine(resultado);
+            }
+
+            
+        }
+    }
+}
